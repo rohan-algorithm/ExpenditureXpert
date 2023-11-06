fee259a5728cc761db75263090c5489f325dd2ac
rohan-algorithm <rohanrao543211@gmail.com>
2023-11-06 20:08:29 +0530

    First Push

---
+++ frontend/src/component/FlexBetween.jsx
@@ -0,0 +1,11 @@
+//to make code re - useble
+const { Box } = require("@mui/material");
+const { styled } = require("@mui/system");
+
+const FlexBetween = styled(Box)({
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+});
+
+export default FlexBetween;
